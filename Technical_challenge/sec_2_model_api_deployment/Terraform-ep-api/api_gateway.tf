08fe9416e50893b444db0de36a9e9b8cea9e983f
Dave Wang <wandave@amazon.com>
2023-04-08 20:54:00 -0500

    update

--- Technical_challenge/sec_2_model_api_deployment/Terraform-ep-api/api_gateway.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_api_gateway_rest_api" "sagemakerapi" {
-  name        = "invoke-sagemaker-API"
-  description = "Rest API gateway for SageMaker endpoint"
-}
-
-output "base_url" {
-  value = "${aws_api_gateway_deployment.sagemakerapi.invoke_url}"
-}
